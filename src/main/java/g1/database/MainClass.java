0fb94a38c71120ef8c099a9a42031e6a8e44cbce
Jonatan Asketorp <jonatan@asketorp.com>
2014-03-29 17:43:48 +0100

    Minor changes.

--- src/main/java/g1/database/MainClass.java
+++ src/main/java/g1/database/MainClass.java
@@ -4,6 +4,7 @@ import java.io.*;
 public class MainClass {
 	private static RDFDatabase rdf;
     public static void main (String args[]) {
+    	/*
     	rdf = new RDFDatabase("lesmis.rdf");
     	String queryString = "SELECT ?x ?y WHERE {?x ?y \"1\"}";
     	String s = rdf.queryDB(queryString);
@@ -11,5 +12,6 @@ public class MainClass {
     	queryString = "SELECT ?x ?y WHERE {?x ?y \"2\"}";
     	s = rdf.queryDB(queryString);
     	System.out.println(s);
+    	*/
     }
 }
