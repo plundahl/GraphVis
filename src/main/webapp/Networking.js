46b246b195fc345524285cadcd3b86b4e6be408f
Jonatan Asketorp <jonatan@asketorp.com>
2014-05-21 10:33:40 +0200

    Minor changes and comments.
    Types, predicates and literals are now sorted.

--- src/main/webapp/Networking.js
+++ src/main/webapp/Networking.js
@@ -9,7 +9,7 @@
 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
 
-var connectTo = "http://localhost:8888/"
+var connectTo = "http://localhost:8888/"; //The address to the server.
 
 /*
 Handles refreshing the SPARQL representation.
@@ -22,12 +22,11 @@ function getSPARQLInterpretation( responseObject ) {
     }
   } else {
     updateTextAreaWithSPARQLQuery(responseObject);
-    //document.getElementById("SPARQLQueryTextArea").value=responseObject;
   }
 }
 
 /*
-Handles sending a pure SPARQL request.
+Handles sending a pure SPARQL request and the subsequent text response.
 */
 function sendSPARQLQuery( responseObject ) {
   if(responseObject===undefined) {
@@ -37,13 +36,12 @@ function sendSPARQLQuery( responseObject ) {
     }
   } else {
     updateTextAreaWithTextResponse(responseObject);
-    //document.getElementById("SPARQLResponseTextArea").value=responseObject;
   }
 }
 
 /*
-Checks and returns the nodes in a format acceptable to vis.js
-This functions assumes that either all objects are for D3 or for vis.
+Checks and returns the nodes in a format acceptable to vis.js.
+Changes here can easily manipulate what is shown by vis.js.
 */
 function verifyJSONForVisjsNodes( returnedObject ) {
   var returnedNodes = returnedObject.nodes;
@@ -51,7 +49,7 @@ function verifyJSONForVisjsNodes( returnedObject ) {
     for(var iterator = 0; iterator<returnedNodes.length; iterator++) {
       returnedNodes[iterator].id = iterator;
       returnedNodes[iterator].label = returnedNodes[iterator].value; //This will write out the nodes label.
-      returnedNodes[iterator].title = returnedNodes[iterator].type;
+      returnedNodes[iterator].title = returnedNodes[iterator].type; //Titles are on mouse-over.
       returnedNodes[iterator].group = returnedNodes[iterator].type;
     }
   }
@@ -61,7 +59,7 @@ function verifyJSONForVisjsNodes( returnedObject ) {
 
 /*
 Checks and returns the edges in a format acceptable to vis.js.
-This functions assumes that either all objects are for D3 or for vis.
+Changes here can easily manipulate what is shown by vis.js.
 */
 function verifyJSONForVisjsEdges( returnedObject ) {
   var returnedEdges = returnedObject.links;
@@ -84,50 +82,82 @@ function verifyJSONForVisjsEdges( returnedObject ) {
 }
 
 /*
-Can be done shorter and easier but meh, serves it purpose, prints out a JSON presentation of the links and nodes ignoring d3js unique values.
+Prints out a JSON respresentation of the interactively created query, this is the String that will be sent to the server.
 */
 function printJSONOutput () {
   var textToTextField="";
   textToTextField += '{"nodes":[';
+  /*
+  For each node
+  */
   for(var iterator = 0; iterator < GraphVisInteraction.nodesInteraction.length; iterator++) {
     var keys = _.keys(GraphVisInteraction.nodesInteraction[iterator]);
     textToTextField += '{';
+    /*
+    Print out all non-key values.
+    */
     for(var key = 0; key < keys.length; key++){
       if(!_.contains(GraphVisInteraction.d3NodeKeyValues, keys[key])) {
         textToTextField += '"'+keys[key]+'":"'+GraphVisInteraction.nodesInteraction[iterator][keys[key]]+'",';
       }
     }
+    /*
+    Print out all literals.
+    */
     textToTextField += '"literals":[';
     var literals = GraphVisInteraction.nodesInteraction[iterator].literals;
     var literalKeys = _.keys(literals);
     for(var literalKey = 0; literalKey< literalKeys.length; literalKey++) {
       textToTextField += '"'+literalKeys[literalKey]+'","'+literals[literalKeys[literalKey]]+'",';
     }
-
+    /*
+    Remove the last ',' if it exists.
+    */
     if(textToTextField.charAt(textToTextField.length -1)==',') {
       textToTextField = textToTextField.slice(0, -1); //"Removes" last character
     }
+    /*
+    End the node.
+    */
     textToTextField += ']},';
   }
+  /*
+  Remove the last ',' if it exists.
+  */
   if(textToTextField.charAt(textToTextField.length - 1)==',') {
     textToTextField = textToTextField.slice(0, -1); //"Removes" last character
   }
+  /*
+  Print the links.
+  */
   textToTextField += '], "links":[';
   for (var iterator = 0; iterator < GraphVisInteraction.links.length; iterator++) {
     var keys = _.keys(GraphVisInteraction.links[iterator]);
     textToTextField += '{';
+    /*
+    Print all non-key values.
+    */
     for(var key = 0; key < keys.length; key++) {
       if(!_.contains(GraphVisInteraction.d3NodeKeyValues, keys[key])) {
         textToTextField += '"'+keys[key]+'":"'+GraphVisInteraction.links[iterator][keys[key]]+'",';
       }
     }
+    /*
+    Print from-to.
+    */
     textToTextField += '"source":'+GraphVisInteraction.links[iterator]["source"].index+','+
       '"target":'+GraphVisInteraction.links[iterator]["target"].index+'},';
   }
+  /*
+  Removes any trailing ','.
+  */
   if(textToTextField.charAt(textToTextField.length - 1)==',') {
     textToTextField = textToTextField.slice(0, -1); //"Removes" last character
   }
   textToTextField += ']}';
+  /*
+  Update the DOM-element, note that databaseOutput is the result of a d3-select.
+  */
   GraphVisInteraction.databaseOutput[0][0].value = textToTextField;
 }
 
@@ -144,7 +174,7 @@ function sendToDatabase( responseObject ) {
       JSON.parse(requestToDatabase);
       requestObjectFromDatabaseWithPrefix( "db/jena", requestToDatabase, sendToDatabase);
     } catch(e) {
-
+      console.log("Could not parse "+requestToDatabase+" to JSON.");
     }
   } else {
     updateVisualization(responseObject);
@@ -153,18 +183,33 @@ function sendToDatabase( responseObject ) {
   }
 }
 
+/*
+Update the TextArea that shows the textual response with the String given as argument.
+*/
 function updateTextAreaWithTextResponse( textResponse ) {
   var textArea = document.getElementById("SPARQLResponseTextArea");
+  /*
+  Should satisfy most browsers.
+  */
   textArea.value=textResponse;
   textArea.innerHTML = textResponse;
 }
 
+/*
+Update the TextArea that shows the SPARQL query with the String given as argument.
+*/
 function updateTextAreaWithSPARQLQuery( SPARQLText ) {
   var textArea = document.getElementById("SPARQLQueryTextArea");
+  /*
+  Should satisfy most browsers.
+  */
   textArea.value = SPARQLText;
   textArea.innerHTML = SPARQLText;
 }
 
+/*
+Used to request something from the database and call the given function with the result.
+*/
 function requestObjectFromDatabaseWithPrefix( prefix, message, callingFunction ) {
   var xhr_object = new XMLHttpRequest();
 	xhr_object.open("POST", connectTo+prefix);
@@ -179,6 +224,9 @@ function requestObjectFromDatabaseWithPrefix( prefix, message, callingFunction )
   xhr_object.send(message);
 }
 
+/*
+Used to request something from the database and call the given function with the result.
+*/
 function requestTextFromDatabaseWithPrefix( prefix, message, callingFunction ) {
   var xhr_object = new XMLHttpRequest();
 	xhr_object.open("POST", connectTo+prefix);
@@ -192,26 +240,44 @@ function requestTextFromDatabaseWithPrefix( prefix, message, callingFunction ) {
   xhr_object.send(message);
 }
 
+/*
+Request predicates from the server.
+*/
 function getPredicatesForInteraction( responseObject ) {
   if(responseObject===undefined) {
     requestObjectFromDatabaseWithPrefix("db-predicates", null, getPredicatesForInteraction);
   } else {
     GraphVisInteraction.availablePredicates = responseObject.edgetypes;
+    GraphVisInteraction.availablePredicates.sort();
   }
 }
 
+/*
+Request types from the server.
+*/
 function getTypesForInteraction( responseObject ) {
   if(responseObject===undefined) {
     requestObjectFromDatabaseWithPrefix("db-types", null, getTypesForInteraction);
   } else {
     GraphVisInteraction.availableTypes = responseObject.types;
+    GraphVisInteraction.availableTypes.sort();
   }
 }
 
+/*
+Request literals from the server.
+*/
 function getLiteralsForInteraction( responseObject ) {
   if(responseObject===undefined) {
     requestObjectFromDatabaseWithPrefix("db-literals", null, getLiteralsForInteraction);
   } else {
     GraphVisInteraction.availableLiterals = responseObject.literals;
+    /*
+    Sort the literals.
+    */
+    var keyValues = _.keys(GraphVisInteraction.availableLiterals);
+    for(var iterator = keyValues.length-1; iterator >= 0; iterator--) {
+      GraphVisInteraction.availableLiterals[keyValues[iterator]].sort();
+    }
   }
 }
